e1830eb3e3902b8a62e114e8a2ebd95b022f534e
Shadid12 <Shadid12@users.noreply.github.com>
2023-05-04 18:31:49 -0400

    update first pass

---
+++ src/App.jsx
@@ -0,0 +1,61 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [url, setUrl] = useState('');
+  const [openQuestion, setQuestion] = useState(false);
+
+  const generateSummary = (type) => {
+  }
+
+  const askQuestionToDoc = () => {}
+
+  return (
+    <div className="container">
+        <div className="card">
+            <form>
+                <label htmlFor="url">🌐 Enter URL: </label>
+                <input 
+                  type="url" 
+                  name="url" 
+                  required 
+                  value={url}
+                  onChange={(e) => setUrl(e.target.value)}
+                />
+                {
+                  openQuestion ? (
+                    <div className="question-container">
+                      <label htmlFor="url"> Write you question </label>
+                      <input 
+                        type="text"
+                      />
+                      <button onClick={askQuestionToDoc}>Submit</button>
+                      <button onClick={e => {
+                        e.preventDefault();
+                        setQuestion(false)
+                      }}>Back</button>
+                    </div>
+                  ) : (
+                    <div className="button-container">
+                      <button onClick={e => {
+                        e.preventDefault();
+                        generateSummary('summary')
+                      }}>Generate Sumary</button>
+                      <button onClick={e => {
+                        e.preventDefault();
+                        generateSummary('point-form')
+                      }}>Generate Sumary (Point Form)</button>
+                      <button onClick={e => {
+                        e.preventDefault();
+                        setQuestion(true)
+                      }}>Ask a Question 🙋</button>
+                    </div>
+                  ) 
+                }
+            </form>
+        </div>
+    </div>
+  )
+}
+
+export default App
